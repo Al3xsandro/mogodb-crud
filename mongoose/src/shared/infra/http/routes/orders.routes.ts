aff2777c0cf47077550d471b8f61da10e58bc807
Al3xsandro <alexs123655@gmail.com>
2022-01-11 16:52:57 -0300

    :tada: add new order route

--- mongoose/src/shared/infra/http/routes/orders.routes.ts
+++ mongoose/src/shared/infra/http/routes/orders.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { CreateOrderController } from "../../../../modules/orders/useCases/CreateOrderController";
+import { CreateOrderController } from "../../../../modules/orders/useCases/createOrder/CreateOrderController";
+import { FindOrdersController } from "../../../../modules/orders/useCases/findOrders/FindOrdersController";
 
 const createOrderController = new CreateOrderController();
+const findOrdersController = new FindOrdersController();
 
 const ordersRoutes = Router();
 
 ordersRoutes.post('/', createOrderController.handle);
+ordersRoutes.get('/', findOrdersController.handle);
 
 export { ordersRoutes };
