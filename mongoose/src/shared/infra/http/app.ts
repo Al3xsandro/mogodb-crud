ba08a77d968ef67fc9e8a22fbb7aa1d05c13501c
Al3xsandro <alexs123655@gmail.com>
2022-01-20 23:17:16 -0300

    :sparkles: add cors

--- mongoose/src/shared/infra/http/app.ts
+++ mongoose/src/shared/infra/http/app.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import 'reflect-metadata';
-import express from "express";
+import cors from 'cors';
 
+import express from "express";
 import 'express-async-errors';
 
 import '../../container/index';
@@ -15,6 +16,7 @@ import { handleErrors } from './middlewares/handleErrors';
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use(routes);
 
 app.use(handleErrors);
