997e4ee449c7e43431a7a63dca941f38efdc1de0
Al3xsandro <alexs123655@gmail.com>
2022-02-01 14:00:59 -0300

    :sparkles: add optional propertie type

--- mongoose/src/modules/products/dtos/ICreateProductDTO.ts
+++ mongoose/src/modules/products/dtos/ICreateProductDTO.ts
@@ -2,8 +2,8 @@ interface ICreateProductDTO {
     name: string;
     price: number;
     quantity: number;
-    stripe_product_id: string;
-    stripe_price_id: string;
+    stripe_product_id?: string;
+    stripe_price_id?: string;
 };
 
 export { ICreateProductDTO };
